7f2d5fb14983e12d8a1d7467dfde104bbc9e45a7
ayon <mohiuddin.310799@gmail.com>
2022-06-18 09:43:25 +0600

    package sql data insertion added

---
+++ sql command/package.sql
@@ -0,0 +1,69 @@
+INSERT INTO PACKAGE (
+  PKG_ID, PKG_NAME, PKG_CHARGE, PKG_TYPE, PKG_DURATION
+)
+  VALUES (
+  'Pkg_001', 'High Squat Rack - Club Combo',700,'Weight Lifting',02
+);
+
+INSERT INTO PACKAGE (
+  PKG_ID, PKG_NAME, PKG_CHARGE, PKG_TYPE, PKG_DURATION
+)
+  VALUES (
+  'Pkg_002', 'Short Squat Rack - Platinum Home Gym',1000,'Weight Lifting',01
+);
+
+
+INSERT INTO PACKAGE (
+  PKG_ID, PKG_NAME, PKG_CHARGE, PKG_TYPE, PKG_DURATION
+)
+  VALUES (
+  'Pkg_003', 'Bar + Black Bumber Plate',750,'Weight Lifting',02
+);
+
+INSERT INTO PACKAGE (
+  PKG_ID, PKG_NAME, PKG_CHARGE, PKG_TYPE, PKG_DURATION
+)
+  VALUES (
+  'Pkg_004', 'Barbell',750,'Beginner Training',02
+);
+
+INSERT INTO PACKAGE (
+  PKG_ID, PKG_NAME, PKG_CHARGE, PKG_TYPE, PKG_DURATION
+)
+  VALUES (
+  'Pkg_005', 'Parallette',1000,'Shoulder Exercise',03
+);
+
+INSERT INTO PACKAGE (
+  PKG_ID, PKG_NAME, PKG_CHARGE, PKG_TYPE, PKG_DURATION
+)
+  VALUES (
+  'Pkg_006', 'Weight Vests',775,'Resistance Training',05
+);
+
+
+INSERT INTO PACKAGE (
+  PKG_ID, PKG_NAME, PKG_CHARGE, PKG_TYPE, PKG_DURATION
+)
+  VALUES (
+  'Pkg_007', 'Strength Bands',890,'Stretching',01
+);
+
+INSERT INTO PACKAGE (
+  PKG_ID, PKG_NAME, PKG_CHARGE, PKG_TYPE, PKG_DURATION
+)
+  VALUES (
+  'Pkg_008', 'Dumbbells & Dumbbell Sets',680,'Muscle Gain',02
+);
+
+INSERT INTO PACKAGE (
+  PKG_ID, PKG_NAME, PKG_CHARGE, PKG_TYPE, PKG_DURATION
+)
+  VALUES (
+  'Pkg_009', 'Climbing Ropes & Battle Ropes',1500,'Physical Training',02
+);
+
+
+
+
+
