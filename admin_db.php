7b9ab05805afe4dc82a841b78a5b1bd964f9027f
Sayed Ar Rafi <51741328+SAR-dev@users.noreply.github.com>
2022-07-20 04:52:49 +0600

    latest

--- admin_db.php
+++ admin_db.php
@@ -345,9 +345,15 @@ if (!$conn) {
 
                           $bn1=  $f_month2 ;
                           $bn2=  $s_month2 ;
-                          $bn3=round(($bn1-$bn2)/$bn1,2)*100 ;
+                          if($bn1!=0)
+                          {
+                            $bn3=round(($bn1-$bn2)/$bn1,2)*100 ;
 
-                          echo"<p class='text-success'>".$bn3."%</p> ";
+                            echo"<p class='text-success'>".$bn3."%</p> ";
+                          }else
+                          {
+                            echo"<p class='text-success'>Revenue of last month doesn't exist</p> ";
+                          };
 
 
 
@@ -410,9 +416,15 @@ if (!$conn) {
 
                           $bn1=  $f_month ;
                           $bn2=  $s_month ;
-                          $bn3=round(($bn1-$bn2)/$bn1,2)*100 ;
+                          if($bn1!=0)
+                          {
+                            $bn3=round(($bn1-$bn2)/$bn1,2)*100 ;
 
-                          echo"<p class='text-success'>".$bn3."%</p> ";
+                            echo"<p class='text-success'>".$bn3."%</p> ";
+                          }else
+                          {
+                            echo"<p class='text-success'>Expendeture of last month doesn't exist</p> ";
+                          }
 
                           echo $ans2 . " BDT";
                           ?>
@@ -431,9 +443,15 @@ if (!$conn) {
 
                              $bn1= $f_month2 - $f_month ;
                              $bn2= $s_month2 - $s_month ;
-                             $bn3=round(($bn1-$bn2)/$bn1,2)*100 ;
-
-                             echo"<p class='text-success'>".$bn3."%</p> ";
+                             if($bn1!=0)
+                             {
+                               $bn3=round(($bn1-$bn2)/$bn1,2)*100 ;
+   
+                               echo"<p class='text-success'>".$bn3."%</p> ";
+                             }else
+                             {
+                               echo"<p class='text-success'>Profit of last month doesn't exist</p> ";
+                             }
 
                           echo $ans - $ans2 . " BDT";
                           
